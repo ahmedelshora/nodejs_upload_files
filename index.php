86bb6f5ad7cb57717fa1474736bc20f2b80590f7
ahmed elshora <ahmedelshora9457@gmail.com>
2019-12-21 16:09:30 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Upload images to server using Node JS</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</head>
+<body>
+    
+
+    <form action="fileupload" method="post" enctype="multipart/form-data">
+        <input type="file" name="filetoupload">
+        <input type="submit" value="Upload">
+    </form>
+
+
+
+
+
+</body>
+</html>
